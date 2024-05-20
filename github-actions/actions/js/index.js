816d5d4d833f402bb3a12779777deb94a10d66ee
Andrew Wasowski <me@wasowski.dev>
2024-05-20 23:10:33 +0200

    New js action

--- github-actions/actions/js/index.js
+++ github-actions/actions/js/index.js
@@ -1,10 +1,35 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function createComment(
+    octokit,
+    owner,
+    repo,
+    issueNumber,
+    body
+  ) {
+    const {data: comment} = await octokit.rest.issues.createComment({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber,
+      body
+    })
+    core.info(`Created comment id '${comment.id}' on issue '${issueNumber}'.`)
+    return comment.id
+  }
+
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const issueNumber = core.getInput('issue-number');
+  console.log(`Issue number ${issueNumber}!`);
+  const token = core.getInput('token');
+  const octokit = github.getOctokit(token);
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  console.log(`Owner ${owner}, repo ${repo}, issueNumber ${issueNumber}!`);
+  const comment = `Hello from the action!`;
+  let id = await createComment(octokit, github.context.repo.owner, github.context.repo.repo, issueNumber, comment);
+  core.info(`Created comment id '${id}' on issue '${issueNumber}'`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
